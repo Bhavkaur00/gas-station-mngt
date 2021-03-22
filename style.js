314e9ef56b026b8497a7c952db19c4b41fb8daa4
aairaverma19 <78393517+aairaverma19@users.noreply.github.com>
2021-03-21 19:12:22 -0700

    Add files via upload

--- style.js
+++ style.js
@@ -1,10 +1,16 @@
 var quotes= [
-    'Remember that happiness is a way of travel, not a destination.',
-    'Life is short, and the world is wide.',
-    'Dare to live the life you have always wanted.'
+    'Buckle Up- You are going on an adventure.',
+    'Not knowing Where I am going is what inspires me the to travel it',
+    'Sometimes all you need is a good friend and a full tank',
+	'Save money and travel further!',
+	'I travel not to go anywhere,but to go. I travel for travel sake. The great affair is to move',
+	'Working from your car is like the private luxury office you never knew you wanted',
+	'Adventure is worthwhile in itself',
+	'Nothing compares to highway driving in the fall'
 ]
 
 function newQuote() {
     var randonNum = Math.floor(Math.random() * (quotes.length));
     document.getElementById('output').innerHTML = quotes[randonNum];
 }
+
